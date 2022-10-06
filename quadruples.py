253075d160d0702368f87569f21fea81ef9714d8
JP Gonzalez <43384928+DrJP99@users.noreply.github.com>
2022-10-06 18:36:01 -0500

    Added comments to quadruples.py

--- quadruples.py
+++ quadruples.py
@@ -15,6 +15,7 @@ class QuadrupleTable:
 	# 	self.listOfQuadruples.append(quad)
 	# 	count += 1
 
+	# Automatically generates the next quadruple
 	def generate(self):
 		opRight = self.pop_operands()
 		typeRight = self.pop_types()
@@ -25,23 +26,24 @@ class QuadrupleTable:
 		operator = self.pop_operators()
 
 		typeRes = self.check_for_mismatch(typeLeft, typeRight, operator)
+		# Normal operators have two operands but '=' has only one so we need to make a different function
 		if (not operator == '='):
 			newQuad = Quadruple(operator, opLeft, opRight, f't{self.temp}')
 			self.listOfQuadruples.append(newQuad)
 			self.push_id_type(f't{self.temp}', typeRes)
-			newQuad.print()
+			# newQuad.print()
 		else:
-			print('generating equal...')
 			self.generate_equal(opRight, opLeft)
 
 		self.temp += 1
 		self.count += 1
 
+	# Generates a quadruple for the '=' operator
 	def generate_equal(self, assignFrom, assignTo):
 		newQuad = Quadruple('=', assignFrom, None, assignTo)
 		self.listOfQuadruples.append(newQuad)
-		newQuad.print()
 
+	# Generates a quadruple for the first NP of the IF
 	def generate_g_if(self):
 		cond = self.pop_operands()
 		typeCond = self.pop_types()
@@ -52,13 +54,15 @@ class QuadrupleTable:
 		else:
 			newQuad = Quadruple('gotoF', cond, None, None)
 			self.listOfQuadruples.append(newQuad)
-			newQuad.print()
+			# newQuad.print()
 			self.stackJumps.append(self.count-1)
 			self.count += 1
-		
+	
+	# Fills the GOTO jump from qFrom to qTarget
 	def fill_jump(self, qFrom, qTarget):
 		self.listOfQuadruples[qFrom].fill_jump(qTarget)
 	
+	# Generates a quadruple for the NP of the ELSE
 	def generate_g_else(self):
 		false = self.pop_jumps()
 		newQuad = Quadruple('goto', None, None, None)
@@ -69,21 +73,25 @@ class QuadrupleTable:
 
 
 	## PUSH ##
+	# Since ID and TYPE are always pushed at the same time, they are combined in this function
 	def push_id_type(self, newId, newType):
 		self.stackOperands.append(newId)
 		self.stackTypes.append(newType)
-		print('operands: ', self.stackOperands)
-		print('types   : ', self.stackTypes)
+		# print('operands: ', self.stackOperands)
+		# print('types   : ', self.stackTypes)
 
+	# Pushes to the stack of operators
 	def push_operator(self, op):
 		self.stackOperators.append(op)
-		print('oprtors : ', self.stackOperators)
+		# print('oprtors : ', self.stackOperators)
 	
+	# Pushes a fake bottom FF to the stack of operators
 	def push_ff(self):
 		self.stackOperators.append('(')
 
 	## POP ##
 
+	# Removes last element and returns it #
 	def pop_operators(self):
 		return self.stackOperators.pop()
 
@@ -102,6 +110,7 @@ class QuadrupleTable:
 	
 	#### GETTERS ####
 	
+	# PEEKS to the last element of the stack without needing to POP it
 	def top_operators(self):
 		if (self.stackOperators):
 			return f'{self.stackOperators[-1]}'
@@ -126,24 +135,28 @@ class QuadrupleTable:
 		else:
 			return None
 	
+	# Returns the current counter
 	def get_curr_counter(self):
 		return self.count
 	
+	# Checks with the ORACLE if the types are allowed together and returns the result type, otherwise returns a type MISMATCH ERROR
 	def check_for_mismatch(self, typeL, typeR, op):
-		print('trying to check types:', typeL, typeR, op)
+		# print('trying to check types:', typeL, typeR, op)
 		try:
 			resType = TIRESIAS[typeL][typeR][op]
-			print ('results in: ', resType)
+			# print ('results in: ', resType)
 			return resType
 		except:
 			print(f'Type mismatch! Impossible to {typeL} {op} {typeR}')
 			exit()
 	
+	# Prints all the quadruples generated
 	def print(self):
 		cont = 0
-		print(len(self.listOfQuadruples))
-		print(self.stackOperators)
-		print(self.stackOperands)
+		# print(len(self.listOfQuadruples))
+		# print(self.stackOperators)
+		# print(self.stackOperands)
+		print("\n~~~>Quadruples generated<~~~")
 		while cont < len(self.listOfQuadruples):
 			print(f'#{cont}:', end='\t')
 			self.listOfQuadruples[cont].print()
@@ -157,8 +170,10 @@ class Quadruple:
 		self.rightOperand = rightOperand
 		self.temp = temp
 	
+	# Fills the jump target of a GOTO quadruple
 	def fill_jump(self, target):
 		self.rightOperand = target
 
+	# Prints an individual quadruple
 	def print(self):
 		print(f'[{self.operator}, {self.leftOperand}, {self.rightOperand}, {self.temp}]', end='\n')
